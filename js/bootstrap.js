284b412cd39b9bb6fc954c3b24648183d1ceab1d
Giovanni Gargiulo <ggargiulo@groupon.com>
2015-04-12 13:35:08 +0100

    fixed js for on off

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2225,15 +2225,6 @@ if ( typeof jQuery === 'undefined') {
 		})
 	})
 
-	$('.btn-toggle').click(function() {
-		$(this).find('.btn').toggleClass('active');
-		
-		$(this).find('.btn').toggleClass('btn-danger');
-
-		$(this).find('.btn').toggleClass('btn-default');
-
-	});
-
 	$('form').submit(function() {
 		alert($(this["options"]).val());
 		return false;
